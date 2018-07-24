3182f509ea6b555d3b47d1563b2fe84974d09b1d
John Eckert <johnteckert@gmail.com>
2018-07-24 12:24:25 -0400

    styled drop down menu

--- src/components/ClassFilter.js
+++ src/components/ClassFilter.js
@@ -3,17 +3,20 @@ import React from "react";
 const ClassFilter = props => {
   // const CLASSES = ["Bard", "Cleric", "Druid", "Paladin", "Ranger", "Sorcerer", "Warlock", "Wizard"];
   return (
-    <select onChange={props.handleCheck} value={props.cl}>
-      <option value="All">All</option>
-      <option value="Bard">Bard</option>
-      <option value="Cleric">Cleric</option>
-      <option value="Druid">Druid</option>
-      <option value="Paladin">Paladin</option>
-      <option value="Ranger">Ranger</option>
-      <option value="Sorcerer">Sorcerer</option>
-      <option value="Warlock">Warlock</option>
-      <option value="Wizard">Wizard</option>
-    </select>
+    <nav>
+      <img src="./owlbear.png" alt="cute owlbear" />
+      <select onChange={props.handleCheck} value={props.cl}>
+        <option value="All">All</option>
+        <option value="Bard">Bard</option>
+        <option value="Cleric">Cleric</option>
+        <option value="Druid">Druid</option>
+        <option value="Paladin">Paladin</option>
+        <option value="Ranger">Ranger</option>
+        <option value="Sorcerer">Sorcerer</option>
+        <option value="Warlock">Warlock</option>
+        <option value="Wizard">Wizard</option>
+      </select>
+    </nav>
   );
 };
 
